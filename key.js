1f3ead2ef63fb25bfffaa7faa345c509f2af703e
FourEyeSamurai <leeson.chris@gmail.com>
2017-04-08 09:30:41 -0500

    Homework Due Commit

--- key.js
+++ key.js
@@ -1,8 +1,6 @@
-console.log('this is loaded');
-
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: '5k44RHpHuSCOt4Z5spUZ0KgEB',
+  consumer_secret: 'ipYODIm849q5XdYKLOR8PfGs4AnCbMESfVJIwbwuGQYrRhmszM>',
+  access_token_key: '2598905376-iejPvhwWWeWgu6dbidA9VJ0EZ9Nz6xh29GPrBVy',
+  access_token_secret: 'lZKvQdlHVYIueNRPbRpjXQByCTsbg0CFvvPGytKnQHCDq',
 }
--- liri.js
+++ liri.js
@@ -1,10 +1,30 @@
 // At the top of the liri.js file, write the code you need to grab the data from keys.js. Then store the keys in a variable.
+ 
+var keys = require("./key.js").twitterKeys;
 
-// Make it so liri.js can take in one of the following commands:
+// var require = require("require");
+
+var request = require("request")
+var twitter = require("twitter");
+var spotify = require("spotify");
 
 // my-tweets
 // node liri.js my-tweets
 // This will show your last 20 tweets and when they were created at in your terminal/bash window.
+
+var client = new twitter({
+  consumer_key: keys.consumer_key,
+  consumer_secret: keys.consumer_secret,
+  access_token_key: keys.access_token_key,
+  access_token_secret: keys.access_token_secret
+});
+
+client.get('tweet_count: 20', function(error, tweets, response) {
+  if(error) throw error;
+  console.log(tweet_count);  // The favorites. 
+  console.log(response);  // Raw response object. 
+});
+
 
 // spotify-this-song
 // node liri.js spotify-this-song '<song name here>'
@@ -15,6 +35,36 @@
 // The album that the song is from
 // if no song is provided then your program will default to
 // "The Sign" by Ace of Base
+
+var nodeArgs = process.argv;
+
+var songName = ""
+
+for (var i = 2; i < nodeArgs.length; i++) {
+
+  if (i > 2 && i < nodeArgs.length) {
+
+    songName = songName + "+" + nodeArgs[i];
+
+  }
+
+  else {
+
+    songName += nodeArgs[i];
+
+  }
+}
+
+
+spotify.search({ artist: "artist", query: songName }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+ 
+    console.log(JSON.parse(data).type)  
+});
+
 
 // movie-this
 // node liri.js movie-this '<movie name here>'
@@ -29,8 +79,41 @@
 //    * Rotten Tomatoes Rating.
 //    * Rotten Tomatoes URL.
 // If the user doesn't type a movie in, the program will output data for the movie 'Mr. Nobody.'
-// If you haven't watched "Mr. Nobody," then you should: http://www.imdb.com/title/tt0485947/
-// It's on Netflix!
+
+var nodeArgs = process.argv;
+
+var movieName = "";
+
+
+for (var i = 2; i < nodeArgs.length; i++) {
+
+  if (i > 2 && i < nodeArgs.length) {
+
+    movieName = movieName + "+" + nodeArgs[i];
+
+  }
+
+  else {
+
+    movieName += nodeArgs[i];
+
+  }
+}
+
+
+
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&r=json";
+
+request(queryUrl, function(error, response, body) {
+
+  // If the request is successful
+  if (!error && response.statusCode === 200) {
+
+  	 console.log("Title: " + JSON.parse(body).Title + "\nRelease Year: " + JSON.parse(body).Year + "\nIMDB Rating: " + 
+  	 	JSON.parse(body).imdbRating + "\nCountry Movie Produced: " + JSON.parse(body).Country + "\nLanguage: " + 
+  	 	JSON.parse(body).Language + "\nPlot: " + JSON.parse(body).Plot + "\nActors: " + JSON.parse(body).Actors);
+  }
+});
 
 // do-what-it-says
 // node liri.js do-what-it-says
